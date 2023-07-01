a210b15b91766337584dc45f6b3be7728894e7e0
Betül Kırçıl <92184238+BetullKircil@users.noreply.github.com>
2023-07-01 15:43:25 +0300

    [FIX][Betul.Kircil][Fixed project dependencies]

---
+++ app/src/main/java/com/betulkircil/crypto_app_clean_arthitecture/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.betulkircil.crypto_app_clean_arthitecture
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.betulkircil.crypto_app_clean_arthitecture.ui.theme.Crypto_App_Clean_ArthitectureTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Crypto_App_Clean_ArthitectureTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Crypto_App_Clean_ArthitectureTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/test/java/com/betulkircil/crypto_app_clean_arthitecture/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.betulkircil.crypto_app_clean_arthitecture
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
